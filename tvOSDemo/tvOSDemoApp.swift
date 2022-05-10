d68f47eb97a3fe195f8902989a10bde375176e24
Vitor Souza <vitorx1280@gmail.com>
2022-05-10 14:05:32 -0300

    Initial Commit

---
+++ tvOSDemo/tvOSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvOSDemoApp.swift
+//  tvOSDemo
+//
+//  Created by Vitor Gledison Oliveira de Souza on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct tvOSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
